9f73796e5d8b1654378c2bae8f03f41df26f2396
Jess Telford <jess+github@jes.st>
2021-05-31 23:08:50 +1000

    better etection of production env

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
-// From https://spectrum.chat/zeit/general/unable-to-import-module-now-launcher-error~2662f0ba-4186-402f-b1db-2e3c43d8689a?m=MTU0MjcyOTQ3NzkxMQ==
 const { PHASE_PRODUCTION_SERVER } =
   process.env.NODE_ENV === 'development'
     ? {}
-    : require('next-server/constants');
+    : !process.env.NOW_REGION
+      ? require('next/constants')
+      : require('next-server/constants');
 
 module.exports = (phase, { defaultConfig }) => {
   if (phase === PHASE_PRODUCTION_SERVER) {
